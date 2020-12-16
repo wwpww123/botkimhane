aa6dd7a11c0763de081ad9bb5134acc2e4d85b68
kimhane <wwpww123@naver.com>
2020-12-16 20:06:59 +0900

    bot upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.token;
+const welcomeChannelName = "joinout";
+const byeChannelName = "joinout";
+const welcomeChannelComment = "어서오세요.";
+const byeChannelComment = "안녕히가세요.";
+
+client.on('ready', () => {
+  console.log('켰다.');
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
+
+client.on('message', (message) => {
+  if(message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login(token);
